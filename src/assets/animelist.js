176254e2f5986f280b99102ea4d8367abc642e35
Miruka <miruka.jp@gmail.com>
2018-04-30 13:27:46 +0900

    がわだけ

--- src/assets/animelist.js
+++ src/assets/animelist.js
@@ -9,33 +9,33 @@ var tv = new Vue ({
 				"charactors": "工藤 新一,江戸川 コナン,毛利 蘭,阿笠 博士,毛利 小五郎,ジン,ウォッカ,目暮 十三",
 				"date": "1996/1/8",
 				"tags": "コナン,新一,蘭,黒づくめ",
-				"comment": ""
+				"comment": "小さくなる"
 			},
 			{
 				"id": 2,
 				"title": "社長令嬢誘拐事件",
-				"importance": "",
+				"importance": "2",
 				"charactors": "江戸川 コナン,",
-				"date": "",
-				"tags": "",
+				"date": "9999/99/99",
+				"tags": "タグ",
 				"comment": ""
 			},
 			{
 				"id": 3,
 				"title": "アイドル密室殺人事件",
-				"importance": "",
+				"importance": "1",
 				"charactors": "",
 				"date": "",
-				"tags": "",
+				"tags": "タグ",
 				"comment": ""
 			},
 			{
-				"id": 4,
+				"id": 9999,
 				"title": "大都会暗号マップ事件",
-				"importance": "",
+				"importance": "0",
 				"charactors": "",
 				"date": "",
-				"tags": "",
+				"tags": "タグ",
 				"comment": ""
 			},
 		]
